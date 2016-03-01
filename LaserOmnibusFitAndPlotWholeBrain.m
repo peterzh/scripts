30ead23b23be7f79544c4669037ce6b139cdc1b0
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2016-03-01 18:03:58 +0000

    Add: creating a map of changes in % choice at C=0 over laser sites.
    Not using the GLM at all. Just looking at the data between laser and
    noLaser trials

--- LaserOmnibusFitAndPlotWholeBrain.m
+++ LaserOmnibusFitAndPlotWholeBrain.m
@@ -244,3 +244,27 @@ Diff = std(sitesP_CRight,[],3);
 hist(Diff); title('std in right contrast sens for all sites');
 % linkaxes(h,'x');
 
+%% (NO GLM) simple map of change in % choice at C=0 over the laser sites
+noLaser_responses = l.data.response(l.data.laserIdx==0 & diff(l.data.contrast_cond,[],2)==0);
+noLaser_p = arrayfun(@(r)(sum(noLaser_responses==r)/length(noLaser_responses)),1:3);
+
+for site = 1:size(l.inactivationSite)
+    Laser_responses = l.data.response(l.data.laserIdx==site & diff(l.data.contrast_cond,[],2)==0);
+    Laser_p(site,:) = arrayfun(@(r)(sum(Laser_responses==r)/length(Laser_responses)),1:3);
+end
+
+pDiff = bsxfun(@minus,Laser_p,noLaser_p);
+figure;
+titles = {'%Left laser - %Left noLaser','%Reft laser - %Reft noLaser','%NG laser - %NG noLaser'};
+for lr=1:3
+    subplot(1,3,lr);
+    scatter(l.inactivationSite(:,2),l.inactivationSite(:,1),400,pDiff(:,lr),'s','filled'); axis equal; colorbar;
+    ylim([-6 4]);
+    xlim([-4 4]);
+    caxis([-1 1]*max(abs(pDiff(:))));
+    title(titles{lr});
+end
+
+cmap = [ones(100,1) linspace(0,1,100)' linspace(0,1,100)';
+    linspace(1,0,100)' linspace(1,0,100)' ones(100,1)];
+colormap(cmap);
