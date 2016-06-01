e1c85173c67cea0d26c5f446fef0775f3cb29014
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2016-06-01 15:17:41 +0100

    cleaned up noLaser model analyses

--- LaserOmnibusModelFreeAnalyses.m
+++ LaserOmnibusModelFreeAnalyses.m
@@ -2,15 +2,12 @@
 g=g.setModel('C50');
 
 for s = 1:numSubjects
-    c0=sum(l(s).data.contrast_cond,2)==0;
-    cL=diff(l(s).data.contrast_cond,[],2)<0;
-    cR=diff(l(s).data.contrast_cond,[],2)>0;
+    
         
     figure('name',expRefs{s,1});
     numSessions = max(l(s).data.sessionID);
     for session = 1:numSessions
-        cF = (1*cL + 2*cR + 3*c0).*(l(s).data.sessionID==session).*(l(s).data.laserIdx==0);
-        
+        D = getrow(l(s).data,l(s).data.laserIdx==0 & l(s).data.sessionID==session);
         params = [sess(s).Biases(session,1),...
                   sess(s).CLeft(session,1),...
                   sess(s).CRight(session,1),...
@@ -18,22 +15,68 @@ for s = 1:numSubjects
                   sess(s).CLeft(session,2),...
                   sess(s).CRight(session,2),...
                   c50_fits{s}(session,:)];
+              
+%         D.p_hats = g.calculatePhat(params,D.contrast_cond);
+        C = unique(D.contrast_cond(:,1));
+        
+        actual = nan(length(C),length(C),3);
+        actual_ci = cell(length(C),length(C));
+        pred = nan(length(C),length(C),3);
+        for cl=1:length(C)
+            for cr=1:length(C)
+                idx = D.contrast_cond(:,1)==C(cl) & D.contrast_cond(:,2)==C(cr);
+                r = D.response(idx);
+                tab=[sum(r==1) sum(r==2) sum(r==3)]';
+                
+                if ~isempty(r)
+                    [p,pci] = binofit(tab,length(r));
+                    actual(cl,cr,1:3) = p;
+                    actual_ci{cl,cr} = pci;
+                end
+                pred(cl,cr,1:3) = g.calculatePhat(params,[C(cl) C(cr)]);%D.p_hats(find(idx,1,'first'),:);
+            end
+        end
+        
+        c0=[]; cE=[]; cL=[]; cR=[];
+        c0_pred = squeeze(pred(1,1,:));
+        c0_act =  squeeze(actual(1,1,:));
+        c0_act_ci = actual_ci{1,1};
+        for r = 1:3
+            pre = pred(:,:,r);
+            act = actual(:,:,r);
+            cE(r,1) = mean(diag(pre(2:end,2:end)));
+            cE(r,2) = nanmean(diag(act(2:end,2:end)));
+            cL(r,1) = mean(pre([2,3,4,7,8,12]));
+            cL(r,2) = nanmean(act([2,3,4,7,8,12]));
+            cR(r,1) = mean(pre([5,9,10,13,14,15]));
+            cR(r,2) = nanmean(act([5,9,10,13,14,15]));
+        end
+
         %Actual choices        
-        for stim = 1:3
-            choices = l(s).data.response(cF==stim);
-            tab=[sum(choices==1) sum(choices==2) sum(choices==3)]';
-            [p,pci]=binofit(tab,length(choices));
-            
+
            
-            subplot(numSessions,3,3*session - 3 + stim);
-            errorbar(1:3,p,p-pci(:,1),pci(:,2)-p,'rs');
-%             plot(p,'s'); 
+            subplot(numSessions,4,4*session - 3);
+            plot(c0_pred); title('C=0');
+            hold on; errorbar(1:3,c0_act,c0_act-c0_act_ci(:,1),c0_act_ci(:,2)-c0_act,'rs'); hold off;
+            subplot(numSessions,4,4*session - 2);
+            plot(cE(:,1)); title('CL=CR');
+            hold on; plot(cE(:,2),'s');hold off;
+            subplot(numSessions,4,4*session - 1);
+            plot(cL(:,1)); title('C=L');
+            hold on; plot(cL(:,2),'s');hold off;
+            subplot(numSessions,4,4*session);
+            plot(cR(:,1)); title('C=R');
+            hold on; plot(cR(:,2),'s');hold off;
+            
+%             errorbar(1:3,p,p-pci(:,1),pci(:,2)-p,'rs');
             ylim([0 1]);
             
-            avStim = mean(l(s).data.contrast_cond(cF==stim,:));
-            hold on;
-            plot(g.calculatePhat(params,avStim));
-            hold off;
-        end
+%             avStim = mean(l(s).data.contrast_cond(cF==stim,:));
+%             avStim = g.calculatePhat(params,l(s).data.contrast_cond(cF==stim,:));
+%             avP = mean(g.calculatePhat(params,l(s).data.contrast_cond(cF==stim,:)));
+%             hold on;
+%             plot(avP);
+%             hold off;
+        
     end
 end
