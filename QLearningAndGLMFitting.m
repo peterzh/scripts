ed07277e52b96622462d9d95b00c7b0935adc16e
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2016-06-07 16:45:52 +0100

    added using w_init from nonDA sessions

--- QLearningAndGLMFitting.m
+++ QLearningAndGLMFitting.m
@@ -62,3 +62,70 @@ for b = 1:length(expRefs)
     savefig(f,fullfile(fig_dir,[expRefs{b} 'modelComparison.fig' ]));
     
 end
+
+%% Fit QGLM to non-DA blocks and use their last W values as the initial values for DA blocks
+%This is to counter the tendency for DA blocks to give weird initial W
+%results
+
+nonDA_expRefs = {'2015-01-16_1_SS031_DA';
+    '2015-01-19_1_SS031_DA';
+    '2015-01-20_1_SS031_DA';
+    '2015-01-21_1_SS031_DA';
+    '2015-01-22_1_SS031_DA';
+    '2015-01-23_3_SS031_DA';
+    '2015-01-26_2_SS031_DA'};
+
+DA_expRefs = {
+    '2015-01-14_1_SS031_DA';
+    '2015-01-14_2_SS031_DA';
+    '2015-01-14_3_SS031_DA';
+    '2015-01-14_4_SS031_DA';
+    '2015-01-15_1_SS031_DA';
+    '2015-01-15_2_SS031_DA';
+    '2015-01-15_3_SS031_DA';
+    '2015-01-15_4_SS031_DA';
+    '2015-01-16_2_SS031_DA';
+    '2015-01-16_4_SS031_DA';
+    '2015-01-16_5_SS031_DA';
+    '2015-01-19_2_SS031_DA';
+    '2015-01-19_3_SS031_DA';
+    '2015-01-19_4_SS031_DA';
+    '2015-01-20_2_SS031_DA';
+    '2015-01-20_3_SS031_DA';
+    '2015-01-20_4_SS031_DA';
+    '2015-01-21_1_SS031_DA';
+    '2015-01-21_2_SS031_DA';
+    '2015-01-21_3_SS031_DA';
+    '2015-01-21_4_SS031_DA';
+    '2015-01-22_2_SS031_DA';
+    '2015-01-22_3_SS031_DA';
+    '2015-01-22_4_SS031_DA';
+    '2015-01-23_1_SS031_DA';
+    '2015-01-23_2_SS031_DA';
+    '2015-01-26_1_SS031_DA';
+    '2015-01-26_3_SS031_DA';
+    };
+
+%For each non-DA session, fit Qs and take the average the last 20 W values
+%then take the average over each session and use that as the initial W for
+%all DA sessions
+w_end = nan(4,length(nonDA_expRefs));
+for b = 1:length(nonDA_expRefs)
+    q = Q(nonDA_expRefs(b)).setModel('aB').fit;
+
+    %get last w values
+    ax=get(gcf,'children');
+    w = cell2mat(get(get(ax(end),'children'),'YData'));
+    close gcf;
+    w_end(:,b)=mean(w(:,end-20:end),2);
+end
+w_init_DA = flipud(mean(w_end,2));
+
+for b = 1:length(DA_expRefs)
+    q = Q(DA_expRefs(b)).setModel('aB');
+    q.preset_winit = w_init_DA;
+    q.fit;
+ax=get(gcf,'children');
+end
+
+
