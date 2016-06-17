3b95db2611229a322a861a9ad1d01fb309bd787f
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2016-06-17 17:12:15 +0100

    added high res coords

--- LaserOmnibusCheckSessions.m
+++ LaserOmnibusCheckSessions.m
@@ -10,7 +10,7 @@ for b = 1:length(expRefs)
     
     try
         p = load(dat.expFilePath(expRefs{b},'parameters','m'));
-        laserGLM(expRefs{b}); %check if any lasermanip problems
+        l = laserGLM(expRefs{b}); %check if any lasermanip problems
         
         switch(expt)
             case 'sparse_bilateral_2D'
@@ -23,12 +23,16 @@ for b = 1:length(expRefs)
                 end
                 
                 block = dat.loadBlock(expRefs{b});
-                if block.numCompletedTrials<100
+                if block.numCompletedTrials<150
                     good(b)=0;
                 end
                 
                 stim = p.parameters.visCueContrast;
                 if ~any(min(stim,[],1)>0)
+                    good(b)=0;
+                end
+                
+                if size(l.inactivationSite,1) < 5
                     good(b)=0;
                 end
                 
@@ -45,11 +49,11 @@ for b = 1:length(expRefs)
 %                 end
 %                 
                 l = laserGLM(expRefs{b});
-                if length(l.data.response)<100
+                if length(l.data.response)<150
                     good(b)=0;
                 end
                 
-                if size(l.inactivationSite,1) < 6
+                if size(l.inactivationSite,1) < 5
                     good(b)=0;
                 end
                 
@@ -67,25 +71,77 @@ for b = 1:length(expRefs)
                 %                     good(b)=0;
                 %                 end
                 %
-                l = laserGLM(expRefs{b});
-                if length(l.data.response)<100
+                if length(l.data.response)<150
                     good(b)=0;
                 end
                 
-                if size(l.inactivationSite,1) < 6
+                if size(l.inactivationSite,1) < 5
                     good(b)=0;
                 end
                 
                  if ~any(l.data.laser(:,2)<0)
                      good(b)=0;
                  end
-                 
-                 if good(b)==1
-                     expRefs{b}
-                 end
+
+            case 'sparse_unilateral_1D_highRes'
+                stim = p.parameters.visCueContrast;
+                if any(min(stim,[],1)>0)
+                    good(b)=0;
+                end
+
+                if length(l.data.response)<150
+                    good(b)=0;
+                end
+                
+                if size(l.inactivationSite,1) < 5
+                    good(b)=0;
+                end
+                
+                if size(l.inactivationSite,1) > 10
+                    good(b)=0;
+                end
+                
+            case 'sparse_unilateral_2D_highRes'
+                
+                good(b)=0;
+                if strcmp(l.sessionLabel,'LineAlongLeftPPC')
+                    good(b)=1;
+                elseif strcmp(l.sessionLabel,'LineAlongRightPPC')
+                    good(b)=1;
+                elseif strcmp(l.sessionLabel,'LineAlongLeftVisual')
+                    good(b)=1;
+                elseif strcmp(l.sessionLabel,'LineAlongRightVisual')
+                    good(b)=1;
+                elseif strcmp(l.sessionLabel,'LineAlongLeftM2')
+                    good(b)=1;
+                elseif strcmp(l.sessionLabel,'LineAlongRightM2')
+                    good(b)=1;
+                end
+                
+            case 'sparse_unilateral_2D_PPC'
+
+                good(b)=0;
+                if strcmp(l.sessionLabel,'LineAlongLeftPPC')
+                    good(b)=1;
+                elseif strcmp(l.sessionLabel,'LineAlongRightPPC')
+                    good(b)=1;
+                end
+                
+            case 'sparse_unilateral_2D_VIS'
+                good(b)=0;
+                
+                if strcmp(l.sessionLabel,'LineAlongLeftVisual')
+                    good(b)=1;
+                elseif strcmp(l.sessionLabel,'LineAlongRightVisual')
+                    good(b)=1;
+                end
                 
             otherwise
                 error('choose something');
+        end
+        
+        if good(b)==1
+            expRefs{b}
         end
         
     catch
